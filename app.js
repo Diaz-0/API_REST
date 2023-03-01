e4e3671d1f4f5ecaf4609b532d6cf75a22ce26cc
Dany <danycambrano1@gmail.com>
2023-03-01 11:34:59 -0600

    modelo de personas

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const app=express();
 
 //importaciones de las rutas de la aplicacion
 const alumnoRoutes=require("./routes/alumnos.routes");
+const personaRoutes=require("./routes/personas.route")
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
@@ -18,6 +19,7 @@ app.use(cors());
 
 //Configuración de las rutas
 app.use(`/api/${API_VERSION}`, alumnoRoutes);
+app.use(`/api/${API_VERSION}`, personaRoutes);
 
 module.exports=app;
 
